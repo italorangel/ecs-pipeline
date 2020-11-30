659bd47d292460447b54b74b9d9dfabe2743d63e
Italo Penaforte <italopenaforte@gmail.com>
2020-11-30 07:30:06 -0300

    creating routing vpc go through

--- modules/vpc/public.tf
+++ modules/vpc/public.tf
@@ -24,6 +24,28 @@ resource "aws_subnet" "public_subnet_us_east_1b" {
   }
 }
 
+resource "aws_subnet" "primary" {
+  vpc_id                  = aws_vpc.cluster_vpc.id
+  cidr_block              = "10.0.0.0/20"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "${var.cluster_name}-public-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "secondary" {
+  vpc_id                  = aws_vpc.cluster_vpc.id
+  cidr_block              = "10.0.16.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "${var.cluster_name}-public-subnet-1b"
+  }
+}
+
 # Associate subnet public_subnet_us_east_1a to public route table
 resource "aws_route_table_association" "public_subnet_us_east_1a_association" {
   subnet_id      = aws_subnet.public_subnet_us_east_1a.id
